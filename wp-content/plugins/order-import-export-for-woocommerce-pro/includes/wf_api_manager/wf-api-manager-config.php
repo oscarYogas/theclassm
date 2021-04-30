1b49cd167f346e5ae5ba96e21ea8d5779a28b072
Oscar Torres <32312985+oscarito7@users.noreply.github.com>
2021-04-30 06:14:38 -0600

    cambios en css

--- wp-content/plugins/order-import-export-for-woocommerce-pro/includes/wf_api_manager/wf-api-manager-config.php
+++ wp-content/plugins/order-import-export-for-woocommerce-pro/includes/wf_api_manager/wf-api-manager-config.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 $product_name = 'ordercsvimportexport'; // name should match with 'Software Title' configured in server, and it should not contains white space
@@ -11,17 +10,3 @@ $plugin_settings_url = admin_url( 'admin.php?page=wf_woocommerce_order_im_ex' );
 include_once ( 'wf_api_manager.php' );
 new WF_API_Manager($product_name, $product_version, $product_slug, $serve_url, $plugin_settings_url);
 ?>
-=======
-<?php
-
-$product_name = 'ordercsvimportexport'; // name should match with 'Software Title' configured in server, and it should not contains white space
-$product_version = '2.4.7';
-$product_slug = 'order-import-export-for-woocommerce-pro/order-import-export.php'; //product base_path/file_name
-$serve_url = 'https://www.webtoffee.com/';
-$plugin_settings_url = admin_url( 'admin.php?page=wf_woocommerce_order_im_ex' );
-
-//include api manager
-include_once ( 'wf_api_manager.php' );
-new WF_API_Manager($product_name, $product_version, $product_slug, $serve_url, $plugin_settings_url);
-?>
->>>>>>> 81d2408ab546c456870bf427d60070682335911c
