d15f1c87b4c5f2f6f5c181b847d9f80bbbc294d9
Oscar Torres <32312985+oscarito7@users.noreply.github.com>
2021-05-03 22:20:13 -0600

    ultimo cambio lunes

--- wp-content/themes/santorotheme/archive-clases_yoga.php
+++ wp-content/themes/santorotheme/archive-clases_yoga.php
@@ -1,22 +1,22 @@
-<?php get_header(); 
-  $_SESSION['landing_before_login'] =  home_url( $wp->request );  
-  $_SESSION['restricted'] =  "no";
-  /*$_SESSION['last_class_seen'] = home_url( $wp->request );*/ 
-  /*$_SESSION['last_class_seen_directo'] = home_url( $wp->request );*/   
-  /*$_SESSION['last_class_plan'] = "Gratis";*/
-  
-  ?>
+<?php get_header();
+$_SESSION['landing_before_login'] = home_url($wp->request);
+$_SESSION['restricted'] = "no";
+/*$_SESSION['last_class_seen'] = home_url( $wp->request );*/
+/*$_SESSION['last_class_seen_directo'] = home_url( $wp->request );*/
+/*$_SESSION['last_class_plan'] = "Gratis";*/
+
+?>
 
 <?php
 $args = array(
     'post_type' => 'clases_yoga',
     'posts_per_page' => 45,
     'facetwp' => true, // we added this
-    //'meta_key'			=> 'visualizaciones',
-	//'orderby'			=> 'meta_value_num',
-	//'order'				=> 'DESC'
+    //'meta_key'            => 'visualizaciones',
+    //'orderby'            => 'meta_value_num',
+    //'order'                => 'DESC'
 );
-$query = new WP_Query( $args );?>
+$query = new WP_Query($args);?>
 
 
     <main>
@@ -41,65 +41,61 @@ $query = new WP_Query( $args );?>
           <?php echo do_shortcode('[facetwp facet="foco"]'); ?>
           <?php echo do_shortcode('[facetwp facet="profesor"]'); ?>
         </div>
-        
+
 
         <div class="filter-result">
-          Tienes <?php echo do_shortcode( '[facetwp counts="true"]' ); ?>
-          <div class="filter-ordenadas">Ordenar por: <?php echo do_shortcode( '[facetwp sort="true"]');?></div>
-          <?php /* <div class="filter-buscar">Si quieres puedes <?php echo do_shortcode('[facetwp facet="buscar"]'); ?></div> */ ?>
+          Tienes <?php echo do_shortcode('[facetwp counts="true"]'); ?>
+          <div class="filter-ordenadas">Ordenar por: <?php echo do_shortcode('[facetwp sort="true"]'); ?></div>
+          <?php /* <div class="filter-buscar">Si quieres puedes <?php echo do_shortcode('[facetwp facet="buscar"]'); ?></div> */?>
         </div>
 
-        
+
         <ul class="video-grid">
 
-        <?php if ($query->have_posts()):  while($query->have_posts()): $query->the_post(); ?>
+        <?php if ($query->have_posts()): while ($query->have_posts()): $query->the_post();?>
 
-        <?php if( get_field('duracion_exacta') != 0 ) { ?>
+				        <?php if (get_field('duracion_exacta') != 0) {?>
 
-          <a href="<?php the_permalink(); ?>">
-            <li class="video-item">
-            <?php if (get_field('plan') == "Gratis") { ?>
-            <div class="video-item-plan alert-success"><?php the_field('plan'); ?></div>
-            <?php } ?>
+				          <a href="<?php the_permalink();?>">
+				            <li class="video-item">
+				            <?php if (get_field('plan') == "Gratis") {?>
+				            <div class="video-item-plan alert-success"><?php the_field('plan');?></div>
+				            <?php }?>
 
-            <?php if (get_field('nuevo') == "Nueva") { ?>
-                <div class="video-item-plan alert-warning"><?php the_field('nuevo'); ?></div>
-            <?php } ?>
+				            <?php if (get_field('nuevo') == "Nueva") {?>
+				                <div class="video-item-plan alert-warning"><?php the_field('nuevo');?></div>
+				            <?php }?>
 
-            <?php if( get_field('imagen_inicio') ) { ?>
-                <img class="video-item-img"  src="<?php the_field('imagen_inicio'); ?>" alt="<?php the_title(); ?>" />
-            <?php } else {  ?>
-              <img class="video-item-img"  src="<?php bloginfo('template_url'); ?>/img/clase-yoga.jpg" alt="<?php the_title(); ?>" />
-            <?php } ?>
-          
-              <h2 class="video-item-title"><?php the_title(); ?></h2>
-              <p class="video-item-filter"> <?php the_field('tipo'); ?> | <?php the_field('duracion_exacta') ?> min | <?php the_field('nivel') ?></p> 
-            
-              <div class="video-item-autor-wrapper">
-                <p class="video-item-autor-name">
-                <?php the_field('visualizaciones') ?> <i class="far fa-eye"></i> | <?php $profesor = get_field('profesor'); echo $profesor['display_name']; ?> 
-                </p>
-              </div>
-            </li>
-          </a> 
-       <?php } ?>
+				            <?php if (get_field('imagen_inicio')) {?>
+				                <img class="video-item-img"  src="<?php the_field('imagen_inicio');?>" alt="<?php the_title();?>" />
+				            <?php } else {?>
+				              <img class="video-item-img"  src="<?php bloginfo('template_url');?>/img/clase-yoga.jpg" alt="<?php the_title();?>" />
+				            <?php }?>
+
+				              <h2 class="video-item-title"><?php the_title();?></h2>
+				              <p class="video-item-filter"> <?php the_field('tipo');?> | <?php the_field('duracion_exacta')?> min | <?php the_field('nivel')?></p>
+
+				              <div class="video-item-autor-wrapper">
+				                <p class="video-item-autor-name">
+				                <?php the_field('visualizaciones')?> <i class="far fa-eye"></i> | <?php $profesor = get_field('profesor');
+            echo $profesor['display_name'];?>
+				                </p>
+				              </div>
+				            </li>
+				          </a>
+				       <?php }?>
 
 
 
 
-          
-        <?php endwhile; endif;?>
-        
-        <?php wp_reset_query(); ?>
-        
-      
+
+				        <?php endwhile;endif;?>
+
+        <?php wp_reset_query();?>
+
       </ul>
-
       <?php echo do_shortcode('[facetwp facet="paginador"]'); ?>
-
-
-
       </div>
     </main>
 
-<?php get_footer(); ?>
+<?php get_footer();?>
